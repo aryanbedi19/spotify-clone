4dbf10e9bf6d25c73ab547c7c2c4e2ec7a4cc20a
Aryan Bedi <aryanbedi@krishans-air.home>
2024-05-16 15:39:10 -0400

    useSound depreciated

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import useSound from "use-sound";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Howl, Howler } from 'howler';
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
@@ -13,88 +13,57 @@ import LikeButton from "./LikeButton";
 import MediaItem from "./MediaItem";
 import Slider from "./Slider";
 
-
 interface PlayerContentProps {
   song: Song;
   songUrl: string;
 }
 
-const PlayerContent: React.FC<PlayerContentProps> = ({ 
-  song, 
-  songUrl
-}) => {
+const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
   const player = usePlayer();
   const [volume, setVolume] = useState(1);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [sound, setSound] = useState<Howl | null>(null);
 
   const Icon = isPlaying ? BsPauseFill : BsPlayFill;
   const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
 
-  const onPlayNext = () => {
-    if (player.ids.length === 0) {
-      return;
-    }
-
-    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
-    const nextSong = player.ids[currentIndex + 1];
-
-    if (!nextSong) {
-      return player.setId(player.ids[0]);
-    }
-
-    player.setId(nextSong);
-  }
-
-  const onPlayPrevious = () => {
-    if (player.ids.length === 0) {
-      return;
-    }
-
-    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
-    const previousSong = player.ids[currentIndex - 1];
-
-    if (!previousSong) {
-      return player.setId(player.ids[player.ids.length - 1]);
-    }
-
-    player.setId(previousSong);
-  }
-
-  const [play, { pause, sound }] = useSound(
-    songUrl,
-    { 
+  useEffect(() => {
+    const newSound = new Howl({
+      src: [songUrl],
       volume: volume,
-      onplay: () => setIsPlaying(true),
+      html5: true, // Force HTML5 audio so it streams. Important for large files!
+      format: ['mp3'],
+      onplay: () => {
+        setIsPlaying(true);
+      },
       onend: () => {
         setIsPlaying(false);
-        onPlayNext();
       },
-      onpause: () => setIsPlaying(false),
-      format: ['mp3']
-    }
-  );
+      onpause: () => {
+        setIsPlaying(false);
+      }
+    });
 
-  useEffect(() => {
-    sound?.play();
-    
+    setSound(newSound);
+
     return () => {
-      sound?.unload();
-    }
-  }, [sound]);
+      newSound.unload();  // Clean up the sound instance
+    };
+  }, [songUrl, volume,]);
 
   const handlePlay = () => {
     if (!isPlaying) {
-      play();
+      sound?.play();
     } else {
-      pause();
+      sound?.pause();
     }
   }
 
   const toggleMute = () => {
-    if (volume === 0) {
-      setVolume(1);
-    } else {
-      setVolume(0);
+    const newVolume = volume === 0 ? 1 : 0;
+    setVolume(newVolume);
+    if (sound) {
+      sound.volume(newVolume);
     }
   }
 
