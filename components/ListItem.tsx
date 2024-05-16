b03bb3f999d293e85e7e36e0ffd26d0acaa65e05
Aryan Bedi <aryanbedi@krishans-air.home>
2024-05-16 14:39:29 -0400

    debugging1

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { FaPlay } from "react-icons/fa";
-import AuthModal from "./AuthModal";
 
 interface ListItemProps {
   image: string;
@@ -19,9 +18,9 @@ const ListItem: React.FC<ListItemProps> = ({
   const router = useRouter();
   
   const onClick = () => {
-    if (!user) {
-      return authModal?.onOpen();
-    }
+    authModal?.onOpen();  
+  };
+  
 
     router.push(href);
   };
